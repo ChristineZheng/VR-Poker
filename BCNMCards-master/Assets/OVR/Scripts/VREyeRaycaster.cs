3c741a9550625ab7fd38b19b99964e1eeecc7a27
ChristineZheng <guominzheng@berkeley.edu>
2017-10-08 14:44:47 -0700

    init

---
+++ BCNMCards-master/Assets/OVR/Scripts/VREyeRaycaster.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using UnityEngine;
+
+namespace VRStandardAssets.Utils
+{
+	// In order to interact with objects in the scene
+	// this class casts a ray into the scene and if it finds
+	// a VRInteractiveItem it exposes it for other classes to use.
+	// This script should be generally be placed on the camera.
+	public class VREyeRaycaster : MonoBehaviour
+	{
+		public event Action<RaycastHit> OnRaycasthit;                   // This event is called every frame that the user's gaze is over a collider.
+
+
+		[SerializeField] private Transform m_Camera;
+		[SerializeField] private LayerMask m_ExclusionLayers;           // Layers to exclude from the raycast.
+		//[SerializeField] private Reticle m_Reticle;                     // The reticle, if applicable.
+		[SerializeField] private VRInput m_VrInput;                     // Used to call input based events on the current VRInteractiveItem.
+		[SerializeField] private bool m_ShowDebugRay;                   // Optionally show the debug ray.
+		[SerializeField] private float m_DebugRayLength = 5f;           // Debug ray length.
+		[SerializeField] private float m_DebugRayDuration = 1f;         // How long the Debug ray will remain visible.
+		[SerializeField] private float m_RayLength = 500f;              // How far into the scene the ray is cast.
+
+
+//		[SerializeField] private float m_DebugRayLength = 5f;           // Debug ray length.
+//		[SerializeField] private float m_DebugRayDuration = 1f;         // How long the Debug ray will remain visible.
+//		[SerializeField] private float m_RayLength = 500f;              // How far into the scene the ray is cast.
+
+		private VRInteractiveItem m_CurrentInteractible;                //The current interactive item
+		private VRInteractiveItem m_LastInteractible;                   //The last interactive item
+
+
+		// Utility for other classes to get the current interactive item
+		public VRInteractiveItem CurrentInteractible
+		{
+			get { return m_CurrentInteractible; }
+		}
+
+
+		private void OnEnable()
+		{
+			m_VrInput.OnClick += HandleClick;
+			m_VrInput.OnDoubleClick += HandleDoubleClick;
+			m_VrInput.OnUp += HandleUp;
+			m_VrInput.OnDown += HandleDown;
+		}
+
+
+		private void OnDisable ()
+		{
+			m_VrInput.OnClick -= HandleClick;
+			m_VrInput.OnDoubleClick -= HandleDoubleClick;
+			m_VrInput.OnUp -= HandleUp;
+			m_VrInput.OnDown -= HandleDown;
+		}
+
+
+		private void Update()
+		{
+			EyeRaycast();
+		}
+
+
+		private void EyeRaycast()
+		{
+			// Show the debug ray if required
+			if (m_ShowDebugRay)
+			{	
+				Debug.DrawRay(m_Camera.position, m_Camera.forward * m_DebugRayLength, Color.blue, m_DebugRayDuration);
+			}
+
+			// Create a ray that points forwards from the camera.
+			Ray ray = new Ray(m_Camera.position, m_Camera.forward);
+			RaycastHit hit;
+
+			// Do the raycast forweards to see if we hit an interactive item
+			if (Physics.Raycast(ray, out hit, m_RayLength, ~m_ExclusionLayers))
+			{
+				VRInteractiveItem interactible = hit.collider.GetComponent<VRInteractiveItem>(); //attempt to get the VRInteractiveItem on the hit object
+				m_CurrentInteractible = interactible;
+
+				// If we hit an interactive item and it's not the same as the last interactive item, then call Over
+				if (interactible && interactible != m_LastInteractible)
+					interactible.Over(); 
+					interactible.transform.LookAt (Vector3.back);
+					
+					//Vector3 pos = interactible.transform.position;
+					//Debug.Log ("vector3: " + pos);
+					//FlipCard (interactible);
+				
+//					Vector3 pos = interactible.transform.position;
+//					// card face camera
+//					if ((-0.5f <= pos.x <= 0.5f) && (-0.5f <= pos.y <= 0.5f) && (-0.5f <= pos.z <= 0.5f)) {
+//						interactible.transform.LookAt (Vector3.back);
+//					}
+//					// card's back face camera
+//					if (pos.x == 0 && pos.y == 0 && pos.z == -1) {
+//						interactible.transform.LookAt (Vector3.zero);
+//					}
+					
+					Debug.Log ("rotate from VREyecaster");
+
+				// Deactive the last interactive item 
+				if (interactible != m_LastInteractible)
+					DeactiveLastInteractible();
+
+				m_LastInteractible = interactible;
+
+//				// Something was hit, set at the hit position.
+//				if (m_Reticle)
+//					m_Reticle.SetPosition(hit);
+
+				if (OnRaycasthit != null)
+					OnRaycasthit(hit);
+			}
+			else
+			{
+				// Nothing was hit, deactive the last interactive item.
+				DeactiveLastInteractible();
+				m_CurrentInteractible = null;
+
+//				// Position the reticle at default distance.
+//				if (m_Reticle)
+//					m_Reticle.SetPosition();
+			}
+		}
+
+		private void FlipCard(VRInteractiveItem item){
+			Vector3 pos = item.transform.position;
+			// card face camera
+			//if ((-0.5f <= pos.x <= 0.5f) && (-0.5f <= pos.y <= 0.5f) && (-0.5f <= pos.z <= 0.5f)) {
+			//if (Mathf.Approximately(0f, pos.x) && Mathf.Approximately(0f, pos.y) && Mathf.Approximately(0f, pos.z)) {
+			if ((-0.5f <= pos.x) && (pos.x <= 0.5f) && (-0.5f <= pos.y) && (pos.y <= 0.5f) && (-0.5f <= pos.z) && (pos.z <= 0.5f)) {
+				item.transform.LookAt (Vector3.back);
+				Debug.Log ("face back");
+			}
+			// card's back face camera
+			//if (Mathf.Approximately(0f, pos.x) && Mathf.Approximately(0f, pos.y) && Mathf.Approximately(-1f, pos.z)) {
+			if ((-0.5f <= pos.x) && (pos.x <= 0.5f) && (-0.5f <= pos.y) && (pos.y <= 0.5f) && (-1.5f <= pos.z) && (pos.z <= 0.5f)) {
+				item.transform.LookAt (Vector3.zero);
+				Debug.Log ("face front");
+			}
+		}
+
+
+		private void DeactiveLastInteractible()
+		{
+			if (m_LastInteractible == null)
+				return;
+
+			m_LastInteractible.Out();
+			m_LastInteractible = null;
+		}
+
+
+		private void HandleUp()
+		{
+			if (m_CurrentInteractible != null)
+				m_CurrentInteractible.Up();
+				//m_CurrentInteractible.transform.LookAt (Vector3.back);
+		}
+
+
+		private void HandleDown()
+		{
+			if (m_CurrentInteractible != null)
+				m_CurrentInteractible.Down();
+				FlipCard (m_CurrentInteractible);
+//				m_CurrentInteractible.transform.LookAt (Vector3.back);
+//				Debug.Log ("down detected");
+		}
+
+
+		private void HandleClick()
+		{
+			if (m_CurrentInteractible != null)
+				m_CurrentInteractible.Click();
+				FlipCard (m_CurrentInteractible);
+//				m_CurrentInteractible.transform.LookAt (Vector3.back);
+//				Debug.Log ("click detected");
+		}
+
+
+		private void HandleDoubleClick()
+		{
+			if (m_CurrentInteractible != null)
+				m_CurrentInteractible.DoubleClick();
+				FlipCard (m_CurrentInteractible);
+//				m_CurrentInteractible.transform.LookAt (Vector3.back);
+//				Debug.Log ("double click detected");
+
+		}
+	}
+}
---
+++ BCNMCards-master/Assets/scripts/Rotation.cs
@@ -0,0 +1,315 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// http://stackoverflow.com/questions/7120845/equivalent-of-tuple-net-4-for-net-framework-3-5
+public class Tuple<T1, T2>
+{
+	public T1 First { get; private set; }
+	public T2 Second { get; private set; }
+	internal Tuple(T1 first, T2 second)
+	{
+		First = first;
+		Second = second;
+	}
+}
+
+public static class Tuple
+{
+	public static Tuple<T1, T2> New<T1, T2>(T1 first, T2 second)
+	{
+		var tuple = new Tuple<T1, T2>(first, second);
+		return tuple;
+	}
+}
+	
+public class Rotation : MonoBehaviour {
+
+	public List<GameObject> Cards;
+	private string suit;
+	private int value;
+
+	private GameObject card;
+	private Vector3 vector;
+	private List<Tuple<Vector3, float>> SuitOrderArray;
+	private List<Tuple<Vector3, float>> RandomOrderArray;
+	private Vector3[] SequentialOrderArray;
+
+//	private bool randomlySorted;
+	private List<Tuple<Vector3, float>> shuffledCardsWithRemoval;
+
+	void Start () {
+
+
+		// S INPUT: Order cards by suits
+		SuitOrderArray = new List<Tuple<Vector3, float>> ();
+		float theta;
+		float radius = 3f;
+		float xPos;
+		float height;
+		float zPos;
+		Vector3 xzPos;
+		// Initialize variable used to calculate xz position
+		float thetaStart = 0;
+
+		for (int i = 0; i < 4; i++) {
+
+			if (i == 0) {
+				thetaStart = 12;	
+			} else if (i == 1) {
+				thetaStart = 25;
+			} else if (i == 2) {
+				thetaStart = 38;
+			} else if (i == 3) {
+				thetaStart = 51;
+			}
+				
+			for (int j = 0; j < 13; j++) {
+
+				// height
+				if (j == 0) {
+					height = -2.5f;
+				} else if (j < 4) {
+					height = -1.25f;
+				} else if (j < 9) {
+					height = 0f;
+				} else if (j < 12) {
+					height = 1.25f;
+				} else {
+					height = 2.5f;
+				}
+
+				// Calculate angle around sphere
+				if (j == 1 | j == 5 | j == 9) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart-1f);
+				} else if (j == 4) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart-2f);
+				} else if (j == 3 | j == 7 | j == 11) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart+1f);
+				} else if (j == 8) {
+					theta = (2 * Mathf.PI / 52) * (thetaStart+2f);
+				} else {
+					theta = (2 * Mathf.PI / 52) * thetaStart;
+				}
+
+				// Calculate x position
+				xPos = Mathf.Cos (theta) * radius;
+				// Calculate z position
+				zPos = Mathf.Sin (theta) * radius;
+				// Add position and height to list
+				xzPos = new Vector3 (xPos, 0, zPos);
+				// S INPUT : includes variable height
+				var suitPosAndHeight = Tuple.New(xzPos, height);
+				SuitOrderArray.Add (suitPosAndHeight);
+
+			}
+
+		}
+
+		// R INPUT: Initialize list to store positions and heights
+		RandomOrderArray = new List<Tuple<Vector3, float>> ();
+		// Initialize variables used to calculate position.
+		theta = 0;
+		radius = 3f;
+		xPos = 3;
+		height = 0;
+		zPos = 0;
+		xzPos = new Vector3 (xPos, height, zPos);
+		var posAndHeight = Tuple.New (xzPos, height);
+		// Add first card's position and height to list
+		RandomOrderArray.Add (posAndHeight);
+
+		// N INPUT : Initialize list to store positions
+		SequentialOrderArray = new Vector3[52];
+		SequentialOrderArray[0] = xzPos;
+
+		// CREATE ARRAYS FOR N AND R INPUTS, INITIALIZE CARDS WITH RANDOM ORDER AND HEIGHT
+		for (int i = 0; i < 52; i++) {
+
+			// Calculate angle around sphere
+			theta = (2 * Mathf.PI / 52) * i;
+			// Calculate x position
+			xPos = Mathf.Cos (theta) * radius;
+			// Calculate y position
+			height = Random.Range(-2.5f, 2.5f);
+			// Calculate z position
+			zPos = Mathf.Sin (theta) * radius;
+			// Add position and height to list
+			xzPos = new Vector3 (xPos, 0, zPos);
+			// S INPUT : includes variable height
+			posAndHeight = Tuple.New(xzPos, height);
+			RandomOrderArray.Add (posAndHeight);
+
+			// N INPUT : without height
+			SequentialOrderArray[i] = xzPos;
+
+			// scale
+			Cards[i].transform.localScale = new Vector3 (10, 10, 10);
+			// face camera
+			Cards[i].transform.LookAt (Vector3.zero);
+			// turn 180 so front of card face camera
+			Cards[i].transform.RotateAround (Vector3.zero, Vector3.up, 180);
+			// apply height
+			Cards[i].transform.position = new Vector3(xPos, height, zPos);
+		}
+
+//		randomlySorted = false;
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+		float x = Input.GetAxis ("Mouse X");
+		float y = Input.GetAxis ("Mouse Y");
+
+		// swipe down -- random order
+		if ((y < 0 && x > -0.5f && x < 0.5f) || Input.GetKeyUp (KeyCode.R)) {
+
+			RandomOrder ();
+
+		}
+		//swipe left -- suit order
+		if ((x < 0 && y > -0.5f && y < 0.5f) || Input.GetKeyUp (KeyCode.S)) {
+
+			SuitOrder ();
+
+		}
+		//swipe right -- sequential order
+		if ((x > 0 && y > -0.5f && y < 0.5f) || Input.GetKeyUp (KeyCode.N)) {
+
+			SequentialOrder ();
+
+		}
+		// rotate cards around sphere in editor
+		if (Input.GetKey (KeyCode.Space)) {
+			RotateAllCardsAroundSphere ();
+		}
+	}
+
+	// Suit Order, at every 90 degrees
+	void SuitOrder() {
+		
+		// find first card
+		// get angle and height of first card
+		Vector3 xzPos = SuitOrderArray [0].First;
+		float height = SuitOrderArray [0].Second;
+		// Move position
+		Cards[0].transform.position = xzPos;
+		float xPos = Cards[0].transform.position.x;
+		float zPos = Cards[0].transform.position.z;
+		iTween.MoveTo (Cards[0], new Vector3 (xPos, height, zPos), 5);
+		// Face camera
+		Cards[0].transform.LookAt (Vector3.zero);
+		// Turn 180 so front of card is facing camera
+		Cards[0].transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+
+		for (int i = 1; i < 52; i++) {
+
+			xzPos = SuitOrderArray [i].First;
+			height = SuitOrderArray [i].Second;
+
+			// Move x and z positions
+			Cards[i].transform.position = xzPos;
+			xPos = Cards[i].transform.position.x;
+			zPos = Cards[i].transform.position.z;
+			iTween.MoveTo (Cards[i], new Vector3 (xPos, height, zPos), 5);
+			// Face camera
+			Cards[i].transform.LookAt (Vector3.zero);
+			// Turn 180 so front of card is facing camera
+			Cards[i].transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+		}
+	}
+
+	// Sequential Order, Same Height
+	void SequentialOrder() {
+
+		// find first card
+		// get angle and height of first card
+		Vector3 xzPos = SequentialOrderArray [0];
+
+		// Move position
+		Cards[0].transform.position = xzPos;
+		float xPos = Cards[0].transform.position.x;
+		float zPos = Cards[0].transform.position.z;
+		iTween.MoveTo (Cards[0], new Vector3 (xPos, 0, zPos), 5);
+
+		// Face camera
+		Cards[0].transform.LookAt (Vector3.zero);
+		// Turn 180 so front of card is facing camera
+		Cards[0].transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+		for (int i = 1; i < 52; i++) {
+
+			xzPos = SequentialOrderArray [i];
+
+			// Move x and z positions
+			Cards[i].transform.position = xzPos;
+			xPos = Cards[i].transform.position.x;
+			zPos = Cards[i].transform.position.z;
+			iTween.MoveTo (Cards[i], new Vector3 (xPos, 0, zPos), 5);
+			// Face camera
+			Cards[i].transform.LookAt (Vector3.zero);
+			// Turn 180 so front of card is facing camera
+			Cards[i].transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+		}
+	}
+
+	// Random Order, Random Height
+	void RandomOrder() {
+
+		// shuffle cards into random order -- working
+		shuffledCardsWithRemoval = RandomOrderArray.OrderBy (a => System.Guid.NewGuid ()).ToList ();;
+
+		// find first card
+		// get angle and height of first card
+		Vector3 xzPos = shuffledCardsWithRemoval [0].First;
+		float height = Random.Range (-2.5f, 2.5f);
+		shuffledCardsWithRemoval.RemoveAt (0);
+
+		// Move position
+		Cards[0].transform.position = xzPos;
+		float xPos = Cards[0].transform.position.x;
+		float zPos = Cards[0].transform.position.z;
+		iTween.MoveTo (Cards[0], new Vector3 (xPos, height, zPos), 5);
+
+		// Face camera
+		Cards[0].transform.LookAt (Vector3.zero);
+		// Turn 180 so front of card is facing camera
+		Cards[0].transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+		for (int i = 1; i < 52; i++) {
+
+			xzPos = shuffledCardsWithRemoval [0].First;
+			height = Random.Range (-2.5f, 2.5f);
+			shuffledCardsWithRemoval.RemoveAt (0);
+
+			// Move x and z positions
+			Cards[i].transform.position = xzPos;
+			xPos = Cards[i].transform.position.x;
+			zPos = Cards[i].transform.position.z;
+			iTween.MoveTo (Cards[i], new Vector3 (xPos, height, zPos), 5);
+			// Face camera
+			Cards[i].transform.LookAt (Vector3.zero);
+			// Turn 180 so front of card is facing camera
+			Cards[i].transform.RotateAround (Vector3.zero, Vector3.up, 180);
+
+		}
+	}
+
+	// Rotate all cards around sphere
+	void RotateAllCardsAroundSphere() {
+
+		Cards[0].transform.RotateAround (Vector3.zero, Vector3.up, (float)1);
+		for (int i = 1; i < 52; i++) {
+
+			Cards[i].transform.RotateAround (Vector3.zero, Vector3.up, (float)1);
+
+		}	
+	}
+	
+}
